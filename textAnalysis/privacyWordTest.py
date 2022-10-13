ffdbb1e1aa203cf55cfd5e819f8721b7fa567819
ruthiep5 <82891285+ruthiep5@users.noreply.github.com>
2022-10-12 20:34:36 -0500

    test

---
+++ textAnalysis/privacyWordTest.py
@@ -0,0 +1,15 @@
+from ruthieAnalysis import *
+
+instaPrivacy = analysisProj("instaPrivacy.txt")
+
+print("number of lines:", instaPrivacy.filelen())
+
+print("number of stanzas:", instaPrivacy.stanzact())
+
+print("number of words per line", instaPrivacy.wordct())
+
+print("number of words", instaPrivacy.total())
+
+impWords = ["personal", "private", ]
+for w in impWords:
+    print(f"number of {w} in insta:", instaPrivacy.privacyWords(w))
