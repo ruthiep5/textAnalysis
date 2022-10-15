14f55b56f0e915dac36166e490364a418ec0ea16
ruthiep5 <82891285+ruthiep5@users.noreply.github.com>
2022-10-14 19:06:54 -0500

    score total

--- textAnalysis/privacyWordTest.py
+++ textAnalysis/privacyWordTest.py
@@ -1,6 +1,6 @@
 from ruthieAnalysis import *
 
-privacyPolicy = analysisProj("facebookPolicy.txt")
+privacyPolicy = analysisProj("instaPrivacy.txt")
 
 print("number of lines:", privacyPolicy.filelen())
 
@@ -11,5 +11,8 @@ print("number of words per line", privacyPolicy.wordct())
 print("number of words", privacyPolicy.total())
 
 impWords = ["personal", "private", "privacy", "protection", "rights", "safe", "guard", "legal", "settings", " lock ", " locks ", "password", "username", "block"]
+numWords = 0
 for w in impWords:
     print(f'number of {w} in insta:', privacyPolicy.privacyWords(w))
+    numWords += privacyPolicy.privacyWords(w)
+print('score = ', numWords)
